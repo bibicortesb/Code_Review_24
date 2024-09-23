2378dd4088e086b7060eef5050222fdb860c4963
BibiCortes <114015620+bibicortesb@users.noreply.github.com>
2024-09-23 14:41:54 +0000

    first submission

---
+++ CR1.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+# Add code below...
+is_multiple_3 = (n%3 == 0)
+print(f'Is {n} a multiple of 3? {is_multiple_3}')
+
+is_multiple_5 = (n%5 == 0)
+print(f'Is {n} a multiple of 5? {is_multiple_5}')
+
+is_multiple_7 = (n%7 == 0)
+print(f'Is {n} a multiple of 7? {is_multiple_7}')
